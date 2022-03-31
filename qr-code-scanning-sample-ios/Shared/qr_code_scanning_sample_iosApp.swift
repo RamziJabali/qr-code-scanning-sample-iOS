6b0b830cbf17c8c8cff39ec4b7db0046cfac86ca
Ramzi Jabali <ramzi@eljabali.org>
2022-03-30 17:01:09 -0700

    initial Commit

---
+++ qr-code-scanning-sample-ios/Shared/qr_code_scanning_sample_iosApp.swift
@@ -0,0 +1,18 @@
+//
+//  qr_code_scanning_sample_iosApp.swift
+//  Shared
+//
+//  Created by Ramzi Jabali on 3/30/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct qr_code_scanning_sample_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
